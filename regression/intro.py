db874f913b296ace7e633d6d9f52df0c60c8b6b9
Pranith Mahanti <pranithmahanti@gmail.com>
2026-04-01 19:57:38 +0530

    Regression #02

--- regression/intro.py
+++ regression/intro.py
@@ -1,9 +1,17 @@
-import pandas
+import pandas as pd
 import yfinance
 import math
 import numpy as np
+from sklearn import preprocessing, svm
+from sklearn.model_selection import cross_validate, train_test_split
+from sklearn.linear_model import LinearRegression
 
-df = yfinance.download('GOOGL', auto_adjust=True)
+from matplotlib import style
+
+df = yfinance.download('GOOGL', period='25y', auto_adjust=True)
+if isinstance(df.columns, pd.MultiIndex):
+    df.columns = df.columns.get_level_values(0)
+
 df = df[['Open', 'High', 'Low', 'Close', 'Volume']]
 
 df['HL_PCT'] = (df['High'] - df['Close']) / df['Close'] * 100
@@ -11,13 +19,47 @@ df['PCT_change'] = (df['Close'] - df['Open']) / df['Open'] * 100
 
 df = df[['Close', 'HL_PCT', 'PCT_change', 'Volume']]
 
-print(df)
+# print(df)
 
 forecast_col = 'Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = int(math.ceil(0.1*len(df)))
+forecast_out = int(math.ceil(0.01*len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
+
+# print(df.head)
+
+X = np.array(df.drop(['label'], axis=1))
+y = np.array(df['label'])
+
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
 df.dropna(inplace=True)
-print(df.head)
+
+X = preprocessing.scale(X)
+
+y = np.array(df['label'])
+
+print(len(X), len(y))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+# print(accuracy)
+
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86000
+
+next_unix = last_unix + one_day
+
+ 
